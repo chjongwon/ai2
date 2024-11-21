6014373b631182c7a601e8e59bf96f5079bec75c
chjongwon <20242529@sdhs.gwe.hs.kr>
2024-11-21 09:16:12 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ def display_right_content(labels):
     # 1st Row - Images based on labels
     for i, label in enumerate(labels[:3]):
         with cols[i]:
-            st.image(f"https://via.placeholder.com/150?text={label}", caption=f"이미지: {label}", use_column_width=True)
+            st.image(f"https://i.ibb.co/60471Wx/e-UCK0e2blksi-YJ9r-Ox5gl-Fiswc-cnr-Vez-Yn-Lh-RKPGfqky7j6tjjk-XVj-OYon-ELox-Mv3oe-YIBhli-XE-qa-H3esyw.webp?text={label}", caption=f"이미지: {label}", use_column_width=True)
 
     # 2nd Row - YouTube Videos based on labels
     for i, label in enumerate(labels[:3]):
